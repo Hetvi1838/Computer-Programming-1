140a416d5e92b738b6a15a18aba76b0218fa0e31
Hetvi1838 <hetvipatel1838@gmail.com>
2025-08-18 10:39:04 +0530

    25BCPT014_24.cpp

--- 25BCPT014_24.cpp
+++ 25BCPT014_24.cpp
@@ -4,6 +4,10 @@ int main()
     int a, b;
     printf("Give two numbers:");
     scanf("%d %d", &a, &b);
-    printf("Two numbers swapped are: %d %d", b, a);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("Two numbers swapped are: %d %d", a, b);
     return 0;
 }
+
